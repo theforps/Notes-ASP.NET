dc74ea5faaf57b2a48a99d47e40e6a0bf612d467
operator <theforps@gmail.com>
2023-02-08 19:36:02 +0300

    add study script

---
+++ Queries.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebNotes
+{
+    public class Queries
+    {
+        public readonly string connectionString = "Server=OPERATOR;Database=Notes;Trusted_Connection=True;MultipleActiveResultSets=True";
+
+        public Queries()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.OpenAsync();
+                SqlCommand command = new SqlCommand();
+
+                //
+                command.CommandText = "CREATE TABLE Users (Id INT PRIMARY KEY IDENTITY, Age INT NOT NULL, Name NVARCHAR(100) NOT NULL)";
+
+
+
+
+
+                command.Connection = connection;
+                command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
