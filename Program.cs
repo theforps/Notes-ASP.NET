57473bc2661f7d27f5be11dcb7734cd4acf15378
theforps <theforps@gmail.com>
2023-03-20 18:54:42 +0300

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using WebNotes.Data;
 
@@ -12,13 +11,10 @@ namespace WebNotes
 
             string connection = builder.Configuration.GetConnectionString("DefaultConnection");
             builder.Services.AddDbContext<NotesDbContext>(options => options.UseSqlServer(connection));
-
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
+            
             if (!app.Environment.IsDevelopment())
             {
                 app.UseHsts();
